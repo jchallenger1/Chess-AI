dd620782fbed4625f9b03717f302e6ea391a35f7
Eren Sulutas <erensulu11@gmail.com>
2021-12-02 19:36:40 -0500

    implemented minimax algorithm
    Co-authored-by: Joshua Challenger <JoshuaChallenger@cmail.carleton.ca>

--- src/ai.js
+++ src/ai.js
@@ -1,24 +1,28 @@
 // Determines the optimal move for the agent using minimax and alpha-beta pruning
 
+let numEvaluations = 0;
+
 function makeRandomMove() {
     var possibleMoves = game.moves()
+
+    numEvaluations = 0;
+    $("#numOfevals").text('0');
 
     // game over
     if (possibleMoves.length === 0) return
 
-    let bestMove = "";
-    let bestMoveEvaluation = -999999;
+    // maximum search depth
     let depth = 3;
 
     // retrieve the move with the best outcome for black
-    let move = max_value(game, depth);
+    let move = getBestMove(game, depth);
+    console.log('best move ' + move);
 
-    var randomIdx = Math.floor(Math.random() * possibleMoves.length)
-    game.move(possibleMoves[randomIdx])
-    let fen = game.fen()
-    board.position(fen)
+    game.move(move);
+    
+    board.position(game.fen());
 
-    makeEvaluation(fen);
+    $("#numOfevals").text(numEvaluations);
 }
 
 // chess piece evaluations
@@ -40,32 +44,102 @@ const evaluations = {
 // Evaluate the position in terms of material
 function makeEvaluation(f) {
     let fen = f.split(' ')[0];
-    console.log(fen)
     let sum = 0;
     for (letter of fen) {
         if (letter in evaluations)
             sum += evaluations[letter]
     }
-    console.log(sum);
+    if (sum === NaN) {
+        console.log("evaluation for board \"" + f + "\" returned a NaN.");
+    }
+    ++numEvaluations;
     return sum;
 }
 
-// function max_value(game, depth) {
-//     let values = []
-//     let possibleMoves = game.moves();
-
-//     // Terminal state
-//     if (depth === 0) return 
-
-//     if ()
-//     for (move of possibleMoves) {
-//         game.move(move);
-//         let evaluation = makeEvaluation(game.fen());
-//         game.undo();
-//     }
-// }
-
-// function min_value(game, depth) {
-//     if (game.game_over()) return false
+// Function that determines the best move through comparing minimax values
+function getBestMove(game, depth) {
+    let possibleMoves = game.moves();
+    let bestMove = possibleMoves[0];
+    let bestMoveEvaluation = -999999;
+    let commonBestMoves = [];
     
-// }
+    for (i in possibleMoves) {
+        let move = possibleMoves[i];
+        game.move(move);
+
+        // Calculate minimum evaluation for the player
+        let eval = minValue(game, depth-1);
+
+        // Update best move
+        if (eval > bestMoveEvaluation) {
+            bestMove = move;
+            bestMoveEvaluation = eval;
+            commonBestMoves = [];
+            commonBestMoves.push(move);
+        }
+        else if (eval === bestMoveEvaluation) {
+            commonBestMoves.push(move);
+        }
+
+        console.log('move ' + move + ' evaluation: ' + eval + ' currentBestMove: ' + bestMove);
+
+        game.undo();
+    }
+
+    console.log(commonBestMoves);
+    // Select random move if all have the same evaluation
+    // if (commonBestMoves.length > 1) {
+    //     let randomIdx = Math.floor(Math.random() * commonBestMoves.length)
+    //     bestMove = commonBestMoves[randomIdx];
+    // }
+
+    return bestMove;
+}
+
+// Return the maximum evaluation for game - represents the agent's move
+function maxValue(game, depth) {
+    let values = [];
+    let possibleMoves = game.moves();
+
+    // Maximum depth achieved
+    if (depth === 0) return makeEvaluation(game.fen());
+
+    // Terminal state
+    if (possibleMoves.length === 0) return makeEvaluation(game.fen());
+    
+    for (move of possibleMoves) {
+        game.move(move);
+        
+        // Calculate minimum evaluation 
+        let eval = minValue(game, depth-1);
+        values.push(eval);
+
+        game.undo();
+    }
+    // console.log(values)
+    return Math.max(...values);
+}
+
+// Return the minimum evaluation for game - represents the player's move
+function minValue(game, depth) {
+    let values = []
+    let possibleMoves = game.moves();
+
+    // Maximum depth achieved
+    if (depth === 0) return makeEvaluation(game.fen());
+
+    // Terminal state
+    if (possibleMoves.length === 0) return makeEvaluation(game.fen());
+
+    for (move of possibleMoves) {
+        game.move(move);
+        
+        // Calculate maximum evaluation 
+        let eval = maxValue(game, depth-1);
+        values.push(eval);
+
+        game.undo();
+    }
+
+    return Math.min(...values);
+}
